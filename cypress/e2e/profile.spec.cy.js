e79d53ede17be1d6fdfeb6cb8dc1712647264092
jcastanov <j.castanov@uniandes.edu.co>
2022-11-13 11:24:50 -0500

    add readme

--- cypress/e2e/profile.spec.cy.js
+++ cypress/e2e/profile.spec.cy.js
@@ -1,44 +1,3 @@
-describe("Go to profile", () => {
-  it('Given I navigate to page "http://localhost:2368/ghost/#/setup"', () => {
-    cy.visit("http://localhost:2368/ghost/#/setup");
-  });
-  it("And I wait for 5 seconds", () => {
-    cy.wait(5000);
-  });
-  it('When I fill register with "title" and "name" and "email@email.com" and "p4ssw0rd.."', () => {
-    cy.url().then((url) => {
-      if (url == "http://localhost:2368/ghost/#/setup") {
-        cy.get("#blog-title").type("cypress-testing");
-        cy.get("#name").type("newUser");
-        cy.get("#email").type("email@email.com");
-        cy.get("#password").type("p4ssw0rd..");
-        cy.wait(5000);
-        cy.get("#ember8").click({ force: true });
-      }
-    });
-  });
-  it('When I fill login with "email@email.com" and "p4ssw0rd.."', () => {
-    cy.url().then((url) => {
-      if (url == "http://localhost:2368/ghost/#/signin") {
-        cy.get("#identification").type("email@email.com");
-        cy.get("#password").type("p4ssw0rd..");
-        cy.get("#ember7").click({ force: true });
-      }
-    });
-  });
-  it("And I try to go to profile", () => {
-    cy.url().then((url) => {
-      cy.wait(5000);
-      cy.get(".gh-user-avatar").click({ force: true, multiple: true });
-      cy.get("#ember51").click({ force: true });
-    });
-  });
-  it("Then I expect to be in my profile", () => {
-    cy.url().then((url) => {
-      expect(url).to.contain("http://localhost:2368/ghost/#/settings/staff");
-    });
-  });
-});
 
 describe("Edit full name in profile", () => {
   it('Given I navigate to page "http://localhost:2368/ghost/#/setup"', () => {
@@ -74,7 +33,7 @@ describe("Edit full name in profile", () => {
   it("And I try to go to profile", () => {
     cy.url().then((url) => {
       cy.get(".gh-user-avatar").click({ force: true, multiple: true });
-      cy.get("#ember51").click({ force: true });
+      cy.get("#ember59").click({ force: true });     // Replace #ember59 according with My Profile button's ember
     });
   });
   it("And I try to set fullname to new-name", () => {
@@ -120,7 +79,7 @@ describe("Edit slug in profile", () => {
   it("And I try to go to profile", () => {
     cy.url().then((url) => {
       cy.get(".gh-user-avatar").click({ force: true, multiple: true });
-      cy.get("#ember51").click({ force: true });
+      cy.get("#ember59").click({ force: true });     // Replace #ember59 according with My Profile button's ember
     });
   });
   it("And I try to set slug to new-slug", () => {
@@ -166,7 +125,7 @@ describe("Edit location in profile", () => {
   it("And I try to go to profile", () => {
     cy.url().then((url) => {
       cy.get(".gh-user-avatar").click({ force: true, multiple: true });
-      cy.get("#ember51").click({ force: true });
+      cy.get("#ember59").click({ force: true });     // Replace #ember59 according with My Profile button's ember
     });
   });
   it("And I try to set location to new-location", () => {
@@ -212,7 +171,7 @@ describe("Edit Bio in profile", () => {
   it("And I try to go to profile", () => {
     cy.url().then((url) => {
       cy.get(".gh-user-avatar").click({ force: true, multiple: true });
-      cy.get("#ember51").click({ force: true });
+      cy.get("#ember59").click({ force: true });     // Replace #ember59 according with My Profile button's ember
     });
   });
   it("And I try to set bio to new-bio", () => {
